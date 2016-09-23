cba02566c2cf26b6943ad224efad838bcb6b1a20
днс1 <1@>
2016-09-23 20:01:13 +0300

    hui

--- HerometrForms/HerometrForms/Form1.cs
+++ HerometrForms/HerometrForms/Form1.cs
@@ -16,5 +16,14 @@ namespace HerometrForms
         {
             InitializeComponent();
         }
+
+        private void btnHui_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MessageBox.Show("Хуй");
+            }
+
+        }
     }
 }
